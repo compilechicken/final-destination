a03b8bb94f8dbef5d045698395fa6bcc92a11cca
Leonard Law <leon.wlaw@gmail.com>
2014-04-13 19:42:52 -0400

    Simple (see: bad) looking camera shake. Looks more like a pulse right now.

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+	public Transform model;
+	Vector3 shake = Vector3.zero;
+	float shakeTime;
+
+	// Use this for initialization
+	void Start () {
+		shakeTime = 0;
+		StartCoroutine(Shake());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			shake.Normalize();
+			Vector3 offset = Quaternion.AngleAxis(Random.value * 360, Vector3.forward) * Vector3.right;
+			shake += offset * 2;
+			shakeTime = 0;
+		}
+	}
+
+	IEnumerator Shake() {
+		while (true) {
+			shakeTime += Time.deltaTime;
+			if (shakeTime < 4) {
+				model.localPosition = Mathf.Sin(shakeTime) * (2 - shakeTime) * shake;
+			}
+			yield return null;
+		}
+	}
+
+
+}

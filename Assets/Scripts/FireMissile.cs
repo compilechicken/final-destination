3aed726f7d7752ef37ff140170e190a4338e87ca
Leonard Law <leon.wlaw@gmail.com>
2014-04-13 18:29:27 -0400

    Missile launch and *VERY* basic tracking. Needs a lot more work.

---
+++ Assets/Scripts/FireMissile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class FireMissile : MonoBehaviour {
+	public Transform target;
+	public Rigidbody projectile;
+	public Transform shotPos;
+	public float shotForce = 1000f;
+	float time = 0;
+	public float delay;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bool shoot = false;
+		time += Time.deltaTime;
+
+		// Are controllers plugged in?
+		if (InputManager.Devices.Count > 0) {
+			shoot = InputManager.Devices[0].LeftTrigger.IsPressed && time > delay;
+		} else {
+			// Debug mode, use keyboard controls.
+			// Left click: shoot standard weapon
+			shoot = Input.GetMouseButton(1) && time > delay;
+		}
+
+		if (shoot) {
+			GameObject missile = (Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody).gameObject;
+			MissileControl missileControl = missile.GetComponent<MissileControl>();
+			missileControl.target = target;
+			missileControl.enabled = true;
+			time = 0;
+		}
+	}
+
+}
---
+++ Assets/Scripts/MissileControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileControl : MonoBehaviour {
+	public float acceleration;
+	public float maxSpeed;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Zoom to target
+		Vector3 toTarget = target.position - transform.position;
+		rigidbody.AddForce(toTarget * acceleration * Time.deltaTime, ForceMode.Acceleration);
+		transform.LookAt(target.position);
+
+	}
+
+}

e480d7b9de9733d53cdf4b94769fbac90e92f508
Leonard Law <leon.wlaw@gmail.com>
2014-04-13 16:20:09 -0400

    Fix nullpointerexceptions when no controllers are plugged in

--- Assets/Scripts/FireLaser.cs
+++ Assets/Scripts/FireLaser.cs
@@ -11,19 +11,25 @@ public class FireLaser : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
-		if(InputManager.Devices[0].RightTrigger.IsPressed && time > delay)
-		{
-			Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
-			shot.AddForce(shotPos.forward * shotForce);
-			time = 0;
+
+		// Are controllers plugged in?
+		if (InputManager.Devices.Count > 0) {
+			if(InputManager.Devices[0].RightTrigger.IsPressed && time > delay)
+			{
+				Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+				shot.AddForce(shotPos.forward * shotForce);
+				time = 0;
+			}
+		} else {
+			// Debug mode, use keyboard controls.
+			// TODO: Add controls
 		}
-		
 	}
 
 }
--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -9,20 +9,26 @@ public class ShipControls : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		//Yaw
-		if(InputManager.Devices[0].LeftStickX.IsNotNull)
-		{
-			transform.Rotate(0,InputManager.Devices[0].LeftStickX.Value,0);
-		}
-		//Pitch
-		if(InputManager.Devices[0].LeftStickY.IsNotNull)
-		{
-			transform.Rotate(-1*InputManager.Devices[0].LeftStickY.Value,0,0);
-		}
-		//Roll
-		if(InputManager.Devices[0].RightStickX.IsNotNull)
-		{
-			transform.Rotate(0,0,InputManager.Devices[0].RightStickX.Value);
+		// Are controllers plugged in?
+		if (InputManager.Devices.Count > 0) {
+			//Yaw
+			if(InputManager.Devices[0].LeftStickX.IsNotNull)
+			{
+				transform.Rotate(0,InputManager.Devices[0].LeftStickX.Value,0);
+			}
+			//Pitch
+			if(InputManager.Devices[0].LeftStickY.IsNotNull)
+			{
+				transform.Rotate(-1*InputManager.Devices[0].LeftStickY.Value,0,0);
+			}
+			//Roll
+			if(InputManager.Devices[0].RightStickX.IsNotNull)
+			{
+				transform.Rotate(0,0,InputManager.Devices[0].RightStickX.Value);
+			}
+		} else {
+			// Debug mode, use keyboard controls.
+			// TODO: Add controls!
 		}
 
 

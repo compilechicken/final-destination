a74d46c21e66b5f290f58c6d63bb0e529329258d
Leonard Law <leon.wlaw@gmail.com>
2014-04-14 23:28:58 -0400

    Better shake code!  * Multiple shakes can happen simultaneously

--- Assets/InControl/Library/VersionInfo.cs
+++ Assets/InControl/Library/VersionInfo.cs
@@ -8,7 +8,7 @@ namespace InControl
 		public const int Major = 1;
 		public const int Minor = 1;
 		public const int Patch = 0;
-		public const int Build = 1783;
+		public const int Build = 1818;
 
 
 		public override string ToString()
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,32 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Ship : MonoBehaviour {
+
+	// Represents a Shake event along the specified vector, with a
+	// remaining duration specified in seconds.
+	private struct ShakeTime {
+		public Vector3 vector;
+		public float time;
+
+		public ShakeTime(Vector3 vector, float time) {
+			this.vector = vector;
+			this.time = time;
+		}
+	}
+
 	public Transform model;
-	Vector3 shake = Vector3.zero;
-	float shakeTime;
+
+	// The list of shake objects that we need to consider
+	LinkedList<ShakeTime> shakes = new LinkedList<ShakeTime>();
 
 	// Use this for initialization
 	void Start () {
-		shakeTime = 0;
 		StartCoroutine(Shake());
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			shake.Normalize();
-			Vector3 offset = Quaternion.AngleAxis(Random.value * 360, Vector3.forward) * Vector3.right;
-			shake += offset * 2;
-			shakeTime = 0;
+			Vector3 shake = 0.5f * (Quaternion.AngleAxis(Random.value * 360, Vector3.forward) * Vector3.right);
+			ShakeTime shakeTime = new ShakeTime(shake, 1);
+			shakes.AddLast(shakeTime);
 		}
 	}
 
 	IEnumerator Shake() {
 		while (true) {
-			shakeTime += Time.deltaTime;
-			if (shakeTime < 4) {
-				model.localPosition = Mathf.Sin(shakeTime) * (2 - shakeTime) * shake;
+			model.localPosition = Vector3.zero;
+
+			// Step through the linked list to figure out what the net
+			// displacement of the ship should be
+			LinkedListNode<ShakeTime> shakeTime = shakes.First;
+			while (shakeTime != shakes.Last) {
+				// Cache the next node so we can move to it
+				LinkedListNode<ShakeTime> nextNode = shakeTime.Next;
+				// Current shake's contribution
+				model.localPosition += (shakeTime.Value.time) * Mathf.Sin(shakeTime.Value.time * 20) * shakeTime.Value.vector;
+
+				if (shakeTime.Value.time <= 0) {
+					// Duration for this shake is complete, delete it
+					shakes.Remove(shakeTime);
+				} else {
+					// Update time remaining in shake
+					ShakeTime updated = new ShakeTime(shakeTime.Value.vector, shakeTime.Value.time - Time.deltaTime);
+					shakeTime.Value = updated;
+				}
+				shakeTime = nextNode;
 			}
 			yield return null;
 		}

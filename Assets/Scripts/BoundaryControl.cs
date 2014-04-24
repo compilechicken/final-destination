2a1979ab230d3d389de5d290d14de47cca85df89
leonwlaw <leon.wlaw@gmail.com>
2014-04-24 13:52:05 -0400

    Basic boundary wall setup

---
+++ Assets/Scripts/BoundaryControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryControl : MonoBehaviour {
+	float distanceToDisplay = 500;
+	Transform[] players = new Transform[2];
+	string PLAYER_TAG = "Player";
+
+	void Start() {
+		GameObject[] players = GameObject.FindGameObjectsWithTag(PLAYER_TAG);
+		for (int i = 0; i < 2; ++i) {
+			this.players[i] = players[i].transform;
+		}
+	}
+
+	void Update() {
+		float closest = Mathf.Infinity;
+		foreach (Transform player in players) {
+			closest = Mathf.Min((player.position - transform.position).magnitude, closest);
+		}
+
+		renderer.enabled = closest < distanceToDisplay;
+	}
+}
